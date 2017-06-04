f0b0898daded35bee87d19a56071871470652ec2
Key-F <kirill2010.97@mail.ru>
2017-06-04 12:46:50 +0300

    +коммент, оставленный под чужой ставкой
    + проверка корректности ввода даты окончания

--- cParser/Form1.cs
+++ cParser/Form1.cs
@@ -145,7 +145,16 @@ namespace cParser
 
         private void button3_Click(object sender, EventArgs e) // WinCheck
         {
-            DateTime End = DateTime.ParseExact(textBox4.Text, "dd-MM-yyyy hh:mm:ss", new CultureInfo("en-US"));
+            DateTime End;
+            try
+            {
+                End = DateTime.ParseExact(textBox4.Text, "dd-MM-yyyy hh:mm:ss", new CultureInfo("en-US"));
+            }
+            catch 
+            {
+                MessageBox.Show("Неверный формат времени окончания");
+                return;
+            }
             //End.Date.ToString("yyyy-M-dd HH:mm:ss.fff");
             richTextBox1.Clear();
              //(checkBox1.CheckState == CheckState.Checked)
@@ -173,15 +182,15 @@ namespace cParser
                 if (i != 0)
                 {
 
-                    if ((Com[i].author != PostAuthor) && (Com[i].vlozhennost == 20) && (Com[i - 1].vlozhennost != 20) && (Com[i - 1].author == PostAuthor) && (Convert.ToDouble(tsnow.TotalHours) > Convert.ToInt32(textBox1.Text)) && (checkBox1.CheckState == CheckState.Checked))
+                    if ((Com[i].author != PostAuthor) && (Com[i].vlozhennost >= 20) && (Com[i - 1].vlozhennost != 20) && (Com[i - 1].author == PostAuthor) && (Convert.ToDouble(tsnow.TotalHours) > Convert.ToInt32(textBox1.Text)) && (checkBox1.CheckState == CheckState.Checked))
                     {
-                        
+
                         richTextBox1.AppendText(" С момента ставки прошло: ", Color.Black);
                         richTextBox1.AppendText(Convert.ToString((int)tsnow.TotalHours), Color.Fuchsia);
                         richTextBox1.AppendText(" часов", Color.Fuchsia);
                         richTextBox1.AppendText(" Possible Winner", Color.Firebrick);
                     }
-                    if ((Com[i].author != PostAuthor) && (Com[i].vlozhennost == 20) && /*(Com[i - 1].vlozhennost != 20) && */(Com[i - 1].dateOfPost > End) && (Convert.ToDouble(ts.TotalHours) > 0) && (checkBox2.CheckState == CheckState.Checked))
+                    if ((Com[i].author != PostAuthor) && (Com[i].vlozhennost >= 20) && /*(Com[i - 1].vlozhennost != 20) && */(Com[i - 1].dateOfPost > End) && (Convert.ToDouble(ts.TotalHours) > 0) && (checkBox2.CheckState == CheckState.Checked))
                     {
 
                         richTextBox1.AppendText(" До окончания: ", Color.Black);
@@ -190,20 +199,23 @@ namespace cParser
                         richTextBox1.AppendText(" Possible Winner", Color.Firebrick);
                     }
                 }
-                else if ((Com[i].author != PostAuthor) && (Com[i].vlozhennost != 20) && (Convert.ToDouble(tsnow.TotalHours) > Convert.ToInt32(textBox1.Text)) && (checkBox1.CheckState == CheckState.Checked))
+                else // Обработка последнего коммента
                 {
-                    richTextBox1.AppendText(" С момента ставки прошло: ", Color.Black);
-                    richTextBox1.AppendText(Convert.ToString((int)tsnow.TotalHours), Color.Fuchsia);
-                    richTextBox1.AppendText(" часов", Color.Fuchsia);
-                    richTextBox1.AppendText(" Possible Winner", Color.Firebrick);                  
-                }
-                else if ((Com[i].author != PostAuthor) && (Com[i].vlozhennost != 20) && (Convert.ToDouble(tsnow.TotalHours) > 0) && (checkBox1.CheckState == CheckState.Checked))
-                {
+                    if ((Com[i].author != PostAuthor) && (Com[i].vlozhennost >= 20) && (Convert.ToDouble(tsnow.TotalHours) > Convert.ToInt32(textBox1.Text)) && (checkBox1.CheckState == CheckState.Checked))
+                    {
+                        richTextBox1.AppendText(" С момента ставки прошло: ", Color.Black);
+                        richTextBox1.AppendText(Convert.ToString((int)tsnow.TotalHours), Color.Fuchsia);
+                        richTextBox1.AppendText(" часов", Color.Fuchsia);
+                        richTextBox1.AppendText(" Possible Winner", Color.Firebrick);
+                    }
+                    if ((Com[i].author != PostAuthor) && (Com[i].vlozhennost >= 20) && (Convert.ToDouble(tsnow.TotalHours) > 0) && (checkBox1.CheckState == CheckState.Checked))
+                    {
 
-                    richTextBox1.AppendText(" До окончания: ", Color.Black);
-                    richTextBox1.AppendText(Convert.ToString((int)ts.TotalHours), Color.Fuchsia);
-                    richTextBox1.AppendText(" часов", Color.Fuchsia);
-                    richTextBox1.AppendText(" Possible Winner", Color.Firebrick);
+                        richTextBox1.AppendText(" До окончания: ", Color.Black);
+                        richTextBox1.AppendText(Convert.ToString((int)ts.TotalHours), Color.Fuchsia);
+                        richTextBox1.AppendText(" часов", Color.Fuchsia);
+                        richTextBox1.AppendText(" Possible Winner", Color.Firebrick);
+                    }
                 }
                 richTextBox1.ScrollToCaret();
                 richTextBox1.AppendText("\n");
